9c7f1d5b1c6423592053801295cfaabf10d6e022
srini <srinivasiyerb@gmail.com>
2013-10-07 14:32:25 +0530

    devogella

--- de.vogella/src/de/vogella/GitTest.java
+++ de.vogella/src/de/vogella/GitTest.java
@@ -3,5 +3,6 @@ package de.vogella;
 public class GitTest {
 	public static void main(String[] args) {
 	    System.out.println("Git is cool");
+	    System.out.println("Gits");
 	  }
 }
--- de.vogella/src/de/vogella/NewClass.java
+++ de.vogella/src/de/vogella/NewClass.java
@@ -3,6 +3,6 @@ package de.vogella;
 public class NewClass {
 	public static void main (String args []){
     	System.out.println("Gits");
-    	 System.out.println("Git is cool");
+    	 
 	}
 }
